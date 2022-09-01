07ca10a351663ebb1723728f55d1b4d2409201c4
victorhrdsilva <victorhrdsilva@gmail.com>
2022-09-01 20:47:29 -0300

    add visual part of new hasbits form

--- src/Components/Habits.js
+++ src/Components/Habits.js
@@ -1,17 +1,71 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 
+function DayButton ({inicial}) {
+    const [isSelectedButton, setIsSelectedButton] = useState(false);
+
+    return ( 
+    <>
+        {isSelectedButton ? <ButtonDaySelected onClick={() => setIsSelectedButton(!isSelectedButton)}>{inicial}</ButtonDaySelected>:
+        <ButtonDay onClick={() => setIsSelectedButton(!isSelectedButton)}>{inicial}</ButtonDay>            
+        }    
+    </>
+    )
+}
 export default function Habits() {
+    const days = [
+        {
+        inicial: "D",
+        day: 1        
+        },
+        {
+        inicial: "S",
+        day: 2        
+        },{
+        inicial: "T",
+        day: 3        
+        },{
+        inicial: "Q",
+        day: 4        
+        },{
+        inicial: "Q",
+        day: 5        
+        },{
+        inicial: "S",
+        day: 6        
+        },{
+        inicial: "S",
+        day: 7        
+        }
+    ]
+    
+    const [isOpenedNewHabits, setIsOpenedNewHabits] = useState(false)
+
     return (
         <Wrapper>
             <Headline>
                 <h2>
                     Meus hábitos
                 </h2>
-                <button>
+                <button onClick={() => setIsOpenedNewHabits(!isOpenedNewHabits)}>
                     +
                 </button>
             </Headline>
+            <NewHabits isOpenedNewHabits={isOpenedNewHabits}>
+                <input type="text" placeholder='nome do hábito' />
+                <WeekDays>
+                    {days.map((item, index) => 
+                    <DayButton
+                        key={index}
+                        inicial={item.inicial} 
+                        />)}
+                </WeekDays>
+                <Buttons>
+                    <button>Cancel</button>
+                    <input type='submit' value='Salvar'></input>
+                </Buttons>
+            </NewHabits>
             <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
         </Wrapper>
     )
@@ -49,5 +103,74 @@ const Headline = styled.div`
         font-size: 25px;
         font-weight: 700;
         border-radius: 5px;
+    }
+`
+const NewHabits = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 85vw;
+    height: ${props => props.isOpenedNewHabits ? '100%' : '0'};
+    overflow: hidden;
+    background-color: var(--secundary-background-color);
+    padding: ${props => props.isOpenedNewHabits ? '16px 0' : '0'};
+    margin-top: 22px;
+    border-radius: 5px;
+    box-sizing: border-box;
+    transition: all 0.07s linear;
+    input {
+        width: 90%;
+        height: 45px;
+        border: 1px solid var(--border-color-input);
+        font-size: 20px;
+        border-radius: 5px;
+        padding-left: 10px;
+        box-sizing: border-box;
+        ::placeholder {
+            padding-left: 10px;
+            color: var(--border-color-input);
+            font-size: 20px;
+        }
+    }
+`
+const WeekDays = styled.div`
+    display: flex;
+    margin: 8px 0;
+    width: 90%;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`
+const ButtonDay = styled.button`
+    width: 35px;
+    height: 35px;
+    border: 1px solid var(--border-color-input);
+    color: var(--border-color-input);
+    background-color: var(--secundary-background-color);
+    border-radius: 5px;
+`
+const ButtonDaySelected = styled(ButtonDay)`
+    background-color: var(--selected-button-color);
+    color: var(--secundary-background-color);
+`
+const Buttons = styled.div`
+    display: flex;
+    align-self: flex-end;
+    margin: 10px 5vw 0 0;
+    button {
+        border: none;
+        background-color: transparent;
+        color: var(--secondary-color);
+        font-size: 18px;
+
+    }
+    input {
+        width: 84px;
+        height: 35px;
+        color: var(--secundary-background-color);
+        background-color: var(--secondary-color);
+        font-size: 18px;
+        padding: 0;
+        margin-left: 10px;
     }
 `
